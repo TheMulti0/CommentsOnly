15bb3dcf52b541d541611a8e0f6d520d93883859
TheMulti0 <multi@codeprecise.com>
2021-02-08 12:44:29 +0200

    Support for supergroups

--- CommentsOnly/Abstractions/MessageFilters.cs
+++ CommentsOnly/Abstractions/MessageFilters.cs
@@ -6,7 +6,10 @@ namespace CommentsOnly
     public static class MessageFilters
     {
         public static readonly MessageFilter PrivateMessage = msg => msg.Chat.Type == ChatType.Private;
+
+        public static readonly MessageFilter GroupMessage =
+            msg => msg.Chat.Type == ChatType.Supergroup || msg.Chat.Type == ChatType.Group;
         
-        public static readonly MessageFilter NewGroupUsers = msg => msg.Chat.Type == ChatType.Group && msg.NewChatMembers?.Any() == true;
+        public static readonly MessageFilter NewGroupUsers = msg => GroupMessage(msg) && msg.NewChatMembers?.Any() == true;
     }
 }
